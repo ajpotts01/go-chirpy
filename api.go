4036c0f99c32f6fc33b9896ba219e36bf5e876da
Alexander Potts <ajpotts01@gmail.com>
2023-08-27 22:24:38 +1000

    Partial refactor of JSON responses

--- api.go
+++ api.go
@@ -65,10 +65,6 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 		CleanedBody string `json:"cleaned_body"`
 	}
 
-	type errorResponse struct {
-		Err string `json:"error"`
-	}
-
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
@@ -120,4 +116,31 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 	return
+}
+
+func validResponse(w http.ResponseWriter, code int, obj interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+
+	resp, err := json.Marshal(obj)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(resp)
+}
+
+func errorResponse(w http.ResponseWriter, code int, errorMsg string) {
+	type errorResponse struct {
+		Err string `json:"error"`
+	}
+
+	errorObj := errorResponse{
+		Err: errorMsg,
+	}
+
+	validResponse(w, code, errorObj)
 }
