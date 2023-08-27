99c3905990bf5f9712f9dea60bbf459d9ef07089
Alexander Potts <ajpotts01@gmail.com>
2023-08-27 15:28:53 +1000

    Basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ajpotts01/go-chirpy
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	corsHandler := cors(mux)
+
+	// Can just do http.ListenAndServe but it may be useful to keep the server object around
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsHandler,
+	}
+
+	log.Printf("Now serving on port: %v", port)
+	log.Fatal(server.ListenAndServe())
+}
