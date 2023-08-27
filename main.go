4e1cf9b56d970edc4730b8872393041d57ec2507
Alexander Potts <ajpotts01@gmail.com>
2023-08-27 16:10:51 +1000

    Endpoints done

--- main.go
+++ main.go
@@ -19,11 +19,23 @@ func cors(next http.Handler) http.Handler {
 	})
 }
 
+func ready(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
+	const dirRoot = "."
+	const healthEndpoint = "/healthz"
+	const appEndpoint = "/app/"
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 	corsHandler := cors(mux)
+
+	mux.HandleFunc(healthEndpoint, ready)
+	mux.Handle(appEndpoint, http.StripPrefix(appEndpoint, http.FileServer(http.Dir(dirRoot))))
 
 	// Can just do http.ListenAndServe but it may be useful to keep the server object around
 	server := &http.Server{
